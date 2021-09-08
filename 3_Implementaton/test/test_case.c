e9da768b75008bcccce9b7e952451904e0a62a7d
99006214 <nagashirisha27@gmail.com>
2021-09-08 10:55:59 +0530

    Add Testcase file

---
+++ 3_Implementaton/test/test_case.c
@@ -0,0 +1,28 @@
+#include "unity.h"
+#include "functions.h"
+
+/* Modify these two lines according to the project */
+#include "functions.h"
+#define PROJECT_NAME "STEPIN_QUIZ-G"
+
+/* Prototypes for all the test functions */
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
